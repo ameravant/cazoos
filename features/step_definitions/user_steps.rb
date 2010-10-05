58c2672941e24608a18c60b36e357dec50da8d22
John Lawrence <john@yourha.com>
2010-10-05 12:57:20 -0700

    added login by person's email to step_definitions

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -15,6 +15,13 @@ Given /^I am logged in as "(.*)" with password "(.*)"$/ do |username, password|
   click_button "Sign in"
 end
 
+Given /^I am logged in as person with email "([^"]*)" with password "([^"]*)"$/ do |email, password|
+  visit new_session_url
+  fill_in "login", :with => Person.find_by_email(email).user.login
+  fill_in "password", :with => password
+  click_button "Sign in"
+end 
+
 Given /^I am logged in as the owner of "(.*)" with password "(.*)"$/ do |org_name, password|
   visit new_session_url
   fill_in "login", :with => Org.find_by_name(org_name).owner.user.login
