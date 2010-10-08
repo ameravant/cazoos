d11c711cb2efedb0733cb19b63005bbdea724791
Macpiece <macpiece@Macpieces-MacBook.local>
2010-10-08 13:52:57 -0700

    adding children to cazoos

--- routes.rb
+++ routes.rb
@@ -3,4 +3,5 @@ namespace :admin do |admin|
   admin.resources :org_types, :as => 'organization_types', :except => :show
   admin.resources :activity_categories, :except => :show
   admin.resources :activities, :except => :show
+  admin.resources :children
 end
